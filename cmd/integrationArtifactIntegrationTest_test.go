8df499a046afbb5f91fd99c9a65f8c9027a33e57
Linda Siebert <linda.siebert@sap.com>
2021-06-24 13:43:07 +0200

    Added first 4 tests
    MessageBodyPath, ContentType, FileExists, No MessageBodyPath

--- cmd/integrationArtifactIntegrationTest_test.go
+++ cmd/integrationArtifactIntegrationTest_test.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/SAP/jenkins-library/pkg/mock"
@@ -21,11 +24,32 @@ func newIntegrationArtifactIntegrationTestTestsUtils() integrationArtifactIntegr
 }
 
 func TestRunIntegrationArtifactIntegrationTest(t *testing.T) {
-	t.Parallel()
+	//t.Parallel()
+	//t.Run("happy path", func(t *testing.T) {
+	//	t.Parallel()
+	//	// init
+	//	config := integrationArtifactIntegrationTestOptions{
+	//		Host:                  "https://demo",
+	//		OAuthTokenProviderURL: "https://demo/oauth/token",
+	//		Username:              "demouser",
+	//		Password:              "******",
+	//		IntegrationFlowID:     "CPI_IFlow_Call_using_Cert",
+	//		Platform:              "cf",
+	//	}
+	//
+	//	utils := newIntegrationArtifactIntegrationTestTestsUtils()
+	//	utils.AddFile("file.txt", []byte("dummy content"))
+	//	httpClient := httpMockCpis{CPIFunction: "IntegrationArtifactGetServiceEndpoint", ResponseBody: ``, TestType: "PositiveAndGetetIntegrationArtifactGetServiceResBody"}
+	//
+	//	// test
+	//	err := runIntegrationArtifactIntegrationTest(&config, nil, utils, &httpClient)
+	//
+	//	// assert
+	//	assert.NoError(t, err)
+	//})
 
-	t.Run("happy path", func(t *testing.T) {
-		t.Parallel()
-		// init
+	t.Run("MessageBodyPath good but no ContentType (ERROR) callIFlowURL", func(t *testing.T) {
+		//init
 		config := integrationArtifactIntegrationTestOptions{
 			Host:                  "https://demo",
 			OAuthTokenProviderURL: "https://demo/oauth/token",
@@ -33,16 +57,95 @@ func TestRunIntegrationArtifactIntegrationTest(t *testing.T) {
 			Password:              "******",
 			IntegrationFlowID:     "CPI_IFlow_Call_using_Cert",
 			Platform:              "cf",
+			MessageBodyPath:       "/file.txt",
+			ContentType:           "",
 		}
 
 		utils := newIntegrationArtifactIntegrationTestTestsUtils()
-		utils.AddFile("file.txt", []byte("dummy content"))
+		utils.AddFile("file.txt", []byte("dummycontent"))
 		httpClient := httpMockCpis{CPIFunction: "IntegrationArtifactGetServiceEndpoint", ResponseBody: ``, TestType: "PositiveAndGetetIntegrationArtifactGetServiceResBody"}
 
-		// test
-		err := runIntegrationArtifactIntegrationTest(&config, nil, utils, &httpClient)
+		//test
+		err := callIFlowURL(&config, nil, utils, &httpClient, "")
 
-		// assert
+		//assert
+		assert.Error(t, err)
+	})
+
+	t.Run("MessageBodyPath and ContentType good but file missing (ERROR) callIFlowURL", func(t *testing.T) {
+		//init
+		config := integrationArtifactIntegrationTestOptions{
+			Host:                  "https://demo",
+			OAuthTokenProviderURL: "https://demo/oauth/token",
+			Username:              "demouser",
+			Password:              "******",
+			IntegrationFlowID:     "CPI_IFlow_Call_using_Cert",
+			Platform:              "cf",
+			MessageBodyPath:       "test.txt",
+			ContentType:           "txt",
+		}
+
+		utils := newIntegrationArtifactIntegrationTestTestsUtils()
+		//no file created here. error expected
+		httpClient := httpMockCpis{CPIFunction: "IntegrationArtifactGetServiceEndpoint", ResponseBody: ``, TestType: "PositiveAndGetetIntegrationArtifactGetServiceResBody"}
+
+		//test
+		err := callIFlowURL(&config, nil, utils, &httpClient, "")
+
+		//assert
+		assert.Error(t, err)
+	})
+
+	t.Run("MessageBodyPath, ContentType, and file good (SUCCESS) callIFlowURL", func(t *testing.T) {
+		dir, err := ioutil.TempDir("", "")
+		defer os.RemoveAll(dir) // clean up
+		assert.NoError(t, err, "Error when creating temp dir")
+		//init
+		config := integrationArtifactIntegrationTestOptions{
+			Host:                  "https://demo",
+			OAuthTokenProviderURL: "https://demo/oauth/token",
+			Username:              "demouser",
+			Password:              "******",
+			IntegrationFlowID:     "CPI_IFlow_Call_using_Cert",
+			Platform:              "cf",
+			MessageBodyPath:       filepath.Join(dir, "test.txt"),
+			ContentType:           "txt",
+		}
+
+		utils := newIntegrationArtifactIntegrationTestTestsUtils()
+		utils.AddFile(config.MessageBodyPath, []byte("dummycontent1")) //have to add a file here to see in utils
+		ioutil.WriteFile(config.MessageBodyPath, []byte("dummycontent2"), 0755)
+		httpClient := httpMockCpis{CPIFunction: "IntegrationArtifactGetServiceEndpoint", ResponseBody: ``, TestType: "PositiveAndGetetIntegrationArtifactGetServiceResBody"}
+
+		//test
+		err = callIFlowURL(&config, nil, utils, &httpClient, "")
+
+		//assert
+		assert.NoError(t, err)
+	})
+
+	t.Run("No MessageBodyPath still works (SUCCESS) callIFlowURL", func(t *testing.T) {
+		//init
+		config := integrationArtifactIntegrationTestOptions{
+			Host:                  "https://demo",
+			OAuthTokenProviderURL: "https://demo/oauth/token",
+			Username:              "demouser",
+			Password:              "******",
+			IntegrationFlowID:     "CPI_IFlow_Call_using_Cert",
+			Platform:              "cf",
+			MessageBodyPath:       "",
+			ContentType:           "txt",
+		}
+
+		utils := newIntegrationArtifactIntegrationTestTestsUtils()
+		//utils.AddFile(config.MessageBodyPath, []byte("dummycontent1")) //have to add a file here to see in utils
+		//ioutil.WriteFile(config.MessageBodyPath, []byte("dummycontent2"), 0755)
+		httpClient := httpMockCpis{CPIFunction: "IntegrationArtifactGetServiceEndpoint", ResponseBody: ``, TestType: "PositiveAndGetetIntegrationArtifactGetServiceResBody"}
+
+		//test
+		err := callIFlowURL(&config, nil, utils, &httpClient, "")
+
+		//assert
 		assert.NoError(t, err)
 	})
 
