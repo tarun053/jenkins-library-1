bb8c84c472575b922db2fb1e3d87175a8ef578dd
Mohit8527 <87068802+Mohit8527@users.noreply.github.com>
2021-08-17 12:41:50 +0530

    Update abapEnvironmentPipeline.groovy

--- vars/abapEnvironmentPipeline.groovy
+++ vars/abapEnvironmentPipeline.groovy
@@ -1,6 +1,6 @@
    void call(parameters) {
     pipeline {
-        agent {jenkins233slave'}
+        agent { label 'jenkins233slave' }
         options {
             skipDefaultCheckout()
         }
